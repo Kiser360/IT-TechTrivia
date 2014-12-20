f74155eb3c8a93e79948502f20bbe426afe9e421
Kiser360 <Kiser360@gmail.com>
2014-12-20 13:37:12 -0600

    Users added and data saved to Firebase

--- auth.js
+++ auth.js
@@ -1,3 +1,7 @@
+
+
+var USERS_LOCATION = "https://brilliant-torch-5122.firebaseio.com/users";
+
 function googleAuth() {
     var FB = new Firebase("https://brilliant-torch-5122.firebaseio.com/");
     FB.authWithOAuthPopup("google", function(error, authData) {
@@ -7,14 +11,41 @@ function googleAuth() {
         }
         else
         {
-            alert("Authenticated: \n\n" + authData);
             toggleAuth();
-            
+            checkIfUserExists(authData);
         }
     
     },{
         remember: "sessionOnly",
-        scope: "profile"
+        scope: "email"
     });
     
+}
+
+function checkIfUserExists(authData) {
+    var usersRef = new Firebase("https://brilliant-torch-5122.firebaseio.com/users");
+    usersRef.child(authData.google.email.split("@")[0]).once('value', function(snapshot) {
+        var exists = (snapshot.val() !== null);
+        if (exists)
+        {
+            console.log(authData);
+            alert("welcome back: ");
+        }
+        else
+        {
+            console.log(authData);
+            alert("New User: ");
+            addUser(authData);
+        }
+    });
+}
+
+function addUser(authData) {
+    usersRef = new Firebase("https://brilliant-torch-5122.firebaseio.com/users/" + authData.google.email.split("@")[0]);
+    usersRef.set({
+        displayName: authData.google.displayName,
+        fullName: authData.google.cachedUserProfile.given_name +" "+ authData.google.cachedUserProfile.family_name,
+        picture: authData.google.cachedUserProfile.picture,
+        email: authData.google.email
+    });
 }
