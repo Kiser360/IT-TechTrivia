4e382bbb067a5010d8728bdfb1261200c3625147
Kiser360 <Kiser360@gmail.com>
2014-12-21 14:57:40 -0600

    Question Modal finished - Adds Q's to Firebase

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 
 
-var USERS_LOCATION = "https://brilliant-torch-5122.firebaseio.com/users";
+var FB_ROOT = "https://brilliant-torch-5122.firebaseio.com";
+var USERS_LOCATION = FB_ROOT + "/users";
+var CURRENT_USER;
 
 function googleAuth() {
     var FB = new Firebase(USERS_LOCATION);
@@ -29,16 +31,15 @@ function checkIfUserExists(authData) {
         if (exists)
         {
             console.log(authData);
-            alert("welcome back: ");
         }
         else
         {
             console.log(authData);
-            alert("New User: ");
             addUser(authData);
         }
         document.getElementById("prof_pic").src = authData.google.cachedUserProfile.picture;
         document.getElementById("prof_name").innerHTML = authData.google.displayName;
+        CURRENT_USER = authData;
         toggleFAB();
     });
 }
@@ -51,4 +52,26 @@ function addUser(authData) {
         picture: authData.google.cachedUserProfile.picture,
         email: authData.google.email
     });
+}
+
+function addQuestion() {
+    var selectedCategory;
+    var radios = document.getElementsByName('category');
+    var answers = document.getElementsByClassName("answer floatLabelInput");
+    for (var i = 0; i < radios.length; i++)
+    {
+        if (radios[i].checked)
+            selectedCategory = radios[i].value;
+    }
+    var quesRef = new Firebase(FB_ROOT + "/questions/" + selectedCategory);
+    var quesID = quesRef.push({
+        author: CURRENT_USER.google.email.split("@")[0],
+        question: document.getElementById("questionText").value,
+        answer1: answers[0].value,
+        answer2: answers[1].value,
+        answer3: answers[2].value,
+        answer4: answers[3].value 
+    });
+    alert("Added question: " + quesID);
+    toggleModal();
 }
